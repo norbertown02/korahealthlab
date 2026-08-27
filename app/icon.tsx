e74de5c17f74a11575d24af9980a542fa2ef95bd
norbertown02 <wiederkehrneto@hotmail.com>
2026-08-27 11:14:49 -0300

    feat: add Kora home screen app icon

---
+++ app/icon.tsx
@@ -0,0 +1,25 @@
+import { ImageResponse } from "next/og";
+import { KoraLogo } from "@/components/kora-logo";
+
+export const size = { width: 512, height: 512 };
+export const contentType = "image/png";
+
+export default function Icon() {
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          width: "100%",
+          height: "100%",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          background: "#3c4536"
+        }}
+      >
+        <KoraLogo />
+      </div>
+    ),
+    size
+  );
+}
